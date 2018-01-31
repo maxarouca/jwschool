4925c4253e878bb952cb4ef9253044d6847688e0
Max Arouca <maximiler@gmail.com>
2018-01-31 18:11:19 -0300

    Install Vue

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Header/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">01</div>
+          <div class="col-md-6">02</div>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
